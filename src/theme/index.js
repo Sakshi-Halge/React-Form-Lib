dd56989e008aee37c6961ecb5a1bee3ea00bd105
Sakshi-Halge <sakshihalge2000@gmail.com>
2025-01-15 23:25:16 +0530

    Added mui and sample custom theme

---
+++ src/theme/index.js
@@ -0,0 +1,16 @@
+import { createTheme } from "@mui/material";
+
+const appTheme = createTheme(
+    {
+        palette: {
+            primary: {
+                main: '#f44336',
+            },
+            secondary: {
+                main: '#3f51b5',
+            },
+        },
+    }
+);
+
+export default appTheme;
